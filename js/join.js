4e498d79d055c28a441c164d4f25cbdc16d7fd4e
LucasB25#7323 <50886682+LucasB25@users.noreply.github.com>
2023-05-11 12:13:21 +0200

    update

--- js/join.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const route = Router();
-
-route.get("/", async (_, res) => {
-    res.redirect(`https://dsc.gg/consteagle`)
-});
-
-module.exports = route;
