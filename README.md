c82ea02fa1378c206383460c4afd3cc5a3167efe
LucasB25#7323 <50886682+LucasB25@users.noreply.github.com>
2023-05-11 12:15:20 +0200

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+<br />
+<div align="center">
+  <a href="https://https://github.com/LucasB25/web-panais">
+    <img src="assets/panais.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Panais</h3>
+
+  <p align="center">
+    <br />
+    <a href="https://discord.gg/fbJFAs43vD">Discord</a>
+    ·
+    <a href="https://discord.com/oauth2/authorize?response_type=code&client_id=707627135577358417&scope=bot+applications.commands&permissions=1644971945463">Add to server</a>
+    ·
+    <a href="https://panaisdev.tk/#command">Commands</a>
+  </p>
+</div>
---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+navToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+function redirectToDiscord() {
+  window.open('https://discord.gg/fbJFAs43vD', '_blank');
+}
+
+function redirectToAddBot() {
+  window.open('https://discord.com/oauth2/authorize?client_id=707627135577358417&permissions=1644971945463&scope=bot%20applications.commands', '_blank');
+}
